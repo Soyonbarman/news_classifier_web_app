2b0b667cfcc9b1dbde0ab6dcf3a40d15bdc63214
Soyon_Barman <118127791+Soyonbarman@users.noreply.github.com>
2024-06-08 00:50:39 +0600

    nltk download

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+nltk.download('punkt')
+nltk.download('stopwords')
 ps = PorterStemmer()
 def clean_text(text):
   text = text.lower()
